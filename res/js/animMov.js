2df21e7e525a078abb270fa314d13adc2b4b668c
Honzak <jfedor06@gmail.com>
2023-04-03 14:28:22 +0200

    todo-door_movement

--- res/js/animMov.js
+++ res/js/animMov.js
@@ -6,59 +6,65 @@ export let roomVisible;
 export default function animMov() {
     let random;
     let interval;
+    let inRoom = 1;
     
     
+    setTimeout(() => {
         function unvIntFunc() {
            
-                
-                random = Math.floor(Math.random() * 3) + 1;
-                if (random == 1) {
+                  
+                  random = Math.floor(Math.random() * 3) + 1;
+                  if (random == 1) {
                     interval = setTimeout(unvIntFunc, 5000);
-
+                    
+                  }
+                  
+                  else if (random == 2) {
+                    interval = setTimeout(unvIntFunc, 5000);
+                  }
+                  
+                  else if (random == 3) {
+                    interval = setTimeout(unvIntFunc, 5000);
+                  }
+                  
+                  randomRoom = Math.floor(Math.random() * 3) + 1;
+                  console.log("Random: " + random)
+                  console.log("Random room: " + randomRoom)
+                  
+                  invBtn1.onclick = () => {
+                    inRoom = 1;
+                    checkAnimPos(1, 1, "res/img/StageHonzak.png", "res/img/emptyStage.png");
+                    console.log(`In room: ${inRoom}`);
+                  }
+                  
+                  invBtn2.onclick = () => {
+                    inRoom = 2;
+                    checkAnimPos(2, 2, "res/img/LHallHonzak.png", "res/img/LHallEmpty.png");
+                    console.log(`In room: ${inRoom}`);
+                  }
+                  
+                  invBtn3.onclick = () => {
+                    inRoom = 3;
+                    checkAnimPos(3, 3, "res/img/RHallHonzak.png", "res/img/RHallEmpty.png");
+                    console.log(`In room: ${inRoom}`);
+                  }
+                  
+                  checkAnimPos(1, 1, "res/img/StageHonzak.png", "res/img/emptyStage.png");
+                  checkAnimPos(2, 2, "res/img/LHallHonzak.png", "res/img/LHallEmpty.png");
+                  checkAnimPos(3, 3, "res/img/RHallHonzak.png", "res/img/RHallEmpty.png");
                 }
                 
-                else if (random == 2) {
-                    interval = setTimeout(unvIntFunc, 15000);
+                function checkAnimPos(roomVal, randomRoomVal, source, emptySource){
+                  if (inRoom == roomVal && randomRoom == randomRoomVal) {
+                    camImg.src = source;
+                  }
+                  else if (inRoom == roomVal && randomRoom != randomRoomVal) {
+                    camImg.src = emptySource
+                  }
                 }
                 
-                else if (random == 3) {
-                    interval = setTimeout(unvIntFunc, 25000);
-                }
-
-                randomRoom = Math.floor(Math.random() * 3) + 1;
-                console.log("Random: " + random)
-                console.log("Random room: " + randomRoom)
-         
-                invBtn1.onclick = () => {
-                    if (randomRoom == 1) {
-                        console.log("Banger")
-                        camImg.src = "res/img/StageHonzak.png";
-                      }
-                    else {
-                        camImg.src = "res/img/emptyStage.png";
-                      }
-                    }
-                    
-                    invBtn2.onclick = () => {
-                      if (randomRoom == 2) {
-                        camImg.src = "res/img/LHallHonzak.png"
-                      }
-                      else{
-                        camImg.src = "res/img/LHallEmpty.png"
-                      }
-                    }
-                    
-                    invBtn3.onclick = () => {
-                      if (randomRoom == 3) {
-                        camImg.src = "res/img/RHallHonzak.png"
-                      }
-                      else{
-                        camImg.src = "res/img/RHallEmpty.png"
-                      }
-                    }
-        }
-
-        unvIntFunc();
+                unvIntFunc();
+              }, 15000);
 
         
 
